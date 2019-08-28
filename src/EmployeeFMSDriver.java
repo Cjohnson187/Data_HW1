741b18c106381882bb32321abbc49e7245128db5
Chris <chrisfjohnson3@gmail.com>
2019-08-28 13:25:10 -0700

    tried to find class to just edit a line but need to read the whole file edit the data then re-write the whole file

--- src/EmployeeFMSDriver.java
+++ src/EmployeeFMSDriver.java
@@ -71,8 +71,28 @@ public class EmployeeFMSDriver implements EmployeeCRUD {
 
     @Override
     public void update(int id, Employee employee) {
+        boolean foundUpdate = false;
+        int lineCount = 0;
         try{
             Scanner in = new Scanner(new FileInputStream(EMPLOYEE_FILENAME));
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                // ignoring deleted employees
+                if (line.charAt(0) == '#') {
+                    lineCount += 1;
+                    continue;
+                    }
+                lineCount +=1;
+                String data[] = line.split(",");
+                int idNum = Integer.parseInt(data[0]);
+                if (idNum == employee.getId()) {
+                    foundUpdate = true;
+                    break;
+                }
+            }
+            in.close();
+
+
 
             // line.replace()
 
@@ -86,6 +106,52 @@ public class EmployeeFMSDriver implements EmployeeCRUD {
 
     @Override
     public void delete(int id) {
+
+        boolean found = false;
+        //Employee tempEmpl = new Employee(0, "#", "#");
+
+        try {
+            PrintStream out = new PrintStream(new FileOutputStream(EMPLOYEE_FILENAME, true));
+            Scanner in = new Scanner(new FileInputStream(EMPLOYEE_FILENAME));
+            int lineCount = 0;
+            while (in.hasNextLine()) {
+                String line = in.nextLine();
+                // ignoring deleted employees
+                if (line.charAt(0) == '#') {
+                    //lineCount += 1; // dnt need it
+                    out.println("#");
+                    continue;
+                }
+                String data[] = line.split(",");
+                int idD = Integer.parseInt(data[0]);
+                Employee temp = new Employee(idD, data[1], data[2]);
+
+                if (id == idD) {
+                    out.println();
+                    found = true;
+                    break;
+                }
+            }
+            in.close();
+        }
+        catch (FileNotFoundException ex) {
+            System.out.println("Employee does not exists!");
+
+        }
+
+        /*
+        if (found)
+            System.out.println("Employee with same id already exists!");
+        else {
+            try {
+                PrintStream out = new PrintStream(new FileOutputStream(EMPLOYEE_FILENAME, true));
+                //out.println(employee);
+                out.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }*/
+
     }
 
     public static void promptCreate(String prompt, EmployeeFMSDriver emp){
@@ -106,8 +172,9 @@ public class EmployeeFMSDriver implements EmployeeCRUD {
     public static void main(String[] args) {
         EmployeeFMSDriver empl = new EmployeeFMSDriver();
         Employee temp = new Employee(0, "none", "none");
-        Employee employee2 = new Employee(3,"Joeseph", "IT");
+        Employee employee2 = new Employee(1,"carl", "IT");
         empl.create(employee2);
+        empl.delete(1);
 
         try {
             temp = empl.read(5);
